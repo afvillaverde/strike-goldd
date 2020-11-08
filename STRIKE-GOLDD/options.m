1509ba8f0b2d78e838d3448bf95c61afd78c4963
Nerea Martínez López <68328150+ML-Nerea@users.noreply.github.com>
2020-11-08 17:10:03 +0100

    Update options.m

--- STRIKE-GOLDD/options.m
+++ STRIKE-GOLDD/options.m
@@ -34,10 +34,10 @@ opts.nnzDerIn   = opts.nnzDerU; % deprecated option
 opts.affine                = 0;     % use algorithm for affine control systems (=1) or not(=0).
 opts.affine_tStage         = 1000;  % max. computation time for the last iteration.
 opts.affine_kmax           = 4;     % max. number of iterations.
-opts.affine_parallel_Lie   = 0;     % use parallel toolbox (=1) or not (=0) to calculate Lie derivatives. 
-opts.affine_parallel_rank  = 0;     % use parallel toolbox (=1) or not (=0) to calculate partial ranks.
+opts.affine_parallel       = 0;     % use parallel toolbox (=1) or not (=0) to calculate partial ranks.
 opts.affine_workers        = 4;     % number of workers for parallel pool.
-opts.affine_graphics       = 1;     % display graphics (=1) or nor (=0)
+opts.affine_graphics       = 1;     % display graphics (=1) or not (=0)
+opts.affine_delete_model   = 1;     % delete affine_model.mat at the end (=1) or not (=0).
 
 %%% (5) User-defined submodels for decomposition (may be left = []): 
 submodels = []; 
@@ -46,9 +46,13 @@ submodels = [];
 %         submodels{2}  = [1 3];
 
 %%% (6) MULTI-EXPERIMENT OPTIONS:
-opts.multiexp           = 0;    % Execute multi-experiment analysis (=1) or not (=0).
-opts.multiexp_numexp    = 3;    % Number of experiments.
-opts.multiexp_user_ics  = 0;    % Set manually the initial conditions for each experiment (=1) or not (=0).
+opts.multiexp              = 0;    % Execute multi-experiment analysis (=1) or not (=0).
+opts.multiexp_numexp       = 3;    % Number of experiments.
+opts.multiexp_user_nnzDerU = 0;    % Set manually the number of nonzero derivatives of the measured inputs (u) for each experiment (=1) or not (=0).
+opts.multiexp_user_nnzDerW = 0;    % Set manually the number of nonzero derivatives of the unmeasured inputs (w) for each experiment (=1) or not (=0).
+opts.multiexp_nnzDerU      = [0 0 0]; % Number of nonzero derivatives of the measured inputs (u) for each experiment; may be 'inf'.
+opts.multiexp_nnzDerW      = [0 0 0]; % Number of nonzero derivatives of the unmeasured inputs (w) for each experiment; may be 'inf'.
+opts.multiexp_user_ics  = 0;       % Set manually the initial conditions for each experiment (=1) or not (=0).
 %- Multi-experiment initial conditions (Rows=variables;Columns:experiments):
 opts.multiexp_ics       = [];
 % syms x10_1 x10_2 x10_3 x20_1 x20_2 x20_3 x30_1 x30_2 x30_3 
