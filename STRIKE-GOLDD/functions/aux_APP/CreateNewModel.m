a5a6ccf40be2ad694ffd0345e308f64277f382cc
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-07-19 11:34:08 +0200

    Add files via upload

--- STRIKE-GOLDD/functions/aux_APP/CreateNewModel.m
+++ STRIKE-GOLDD/functions/aux_APP/CreateNewModel.m
@@ -1,18 +1,16 @@
 function CreateNewModel()
 clear all
-syms Tu Ti V
-x = transpose([Tu Ti V]);
-syms lambda rho N delta c
-p = transpose([lambda rho N delta c]);
-h = transpose([V, Tu+Ti]);
-syms eta
-u = [eta];
- 
-w = [];
-f = [ lambda-rho*Tu-eta*Tu*V;
- eta*Tu*V-delta*Ti;
-N*delta*Ti-c*V];
+syms x1 x2 x3
+x = transpose([x1 x2 x3]);
+syms p1 p2 p3 p4 p5
+p = transpose([p1 p2 p3 p4 p5]);
+h = transpose([x3, x2+x1]);
+u = [];syms u1
+w = [u1];
+f = [ p1 - p2*x1 - u1*x1*x3;
+ u1*x1*x3 - p4*x2;
+p3*p4*x2 - p5*x3];
 ics  = [[000]];
 known_ics = [1 ];
-save("C:\Users\xabor\Downloads\APP\strike-goldd-dev\STRIKE-GOLDD\models\HIV","x","p","u","w","h","f","ics","known_ics");
+save("C:\Users\xabor\Downloads\strike-goldd-master\STRIKE-GOLDD\models\EXAMPLE","x","p","u","w","h","f","ics","known_ics");
 end
