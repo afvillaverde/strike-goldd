782d2796ece1c1e007e2ac7335dc123f63854daf
SandraDiazSeoane <sandra.diaz@uvigo.gal>
2023-04-14 13:18:24 +0200

    Update OutputSLP.m
    Adaptation of the code to work with systems that have known parameters in the output and don't appear in the state functions.

--- STRIKE-GOLDD/functions/aux_Prob_Obs_Test/OutputSLP.m
+++ STRIKE-GOLDD/functions/aux_Prob_Obs_Test/OutputSLP.m
@@ -12,5 +12,16 @@ SLPOut=subs(SLPOut,k,k_esp);
 SLPOut=subs(SLPOut,x,Sol);
 SLPOut=subs(SLPOut,VSMOut,SM);
 
+kout=conj(symvar(SLPOut))';
+[ch, original_index] = ismember(t,kout); 
+if ch~=0
+    kout(original_index)=[]; 
+end
+if ~isempty(kout)
+    kout_esp = randi([0,Myprime],length(kout),1);
+    SLPOut=subs(SLPOut,kout,kout_esp);
+    SLPOut=subs(SLPOut,kout,kout_esp);
+end
+
 SLPOut=taylor(SLPOut,t,'order',Order); 
 SLPOut=polmod(SLPOut,Myprime,logic); 
